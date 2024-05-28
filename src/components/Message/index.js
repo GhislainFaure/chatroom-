eea9cda5864226fbe766e19db78d3c00d2ba742d
GhislainFaure <ghislainfaure06@gmail.com>
2024-05-28 23:30:14 +0200

    injection de la classe css message--is-mine si l'auteur et le pseudo sont egaux

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -6,7 +6,7 @@ export default function Message({ author, content }) {
   const { pseudo } = useSelector((state) => state.messages);
 
   return (
-    <div className={author === pseudo ? "message--is-mine" : "message"}>
+    <div className={author === pseudo ? "message message--is-mine" : "message"}>
       <p className={"message__author"}>{author}</p>
       <p className="message__content">{content}</p>
     </div>
