2565f42cab091abd798eda47d1e2bf1b0da59c37
GhislainFaure <ghislainfaure06@gmail.com>
2024-05-28 23:22:45 +0200

    CSS pour mes messages

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./style.scss";
+import { useSelector } from "react-redux";
 
 export default function Message({ author, content }) {
+  const { pseudo } = useSelector((state) => state.messages);
+
   return (
-    <div className="message">
-      <p className="message__author">{author}</p>
+    <div className={author === pseudo ? "message--is-mine" : "message"}>
+      <p className={"message__author"}>{author}</p>
       <p className="message__content">{content}</p>
     </div>
   );
