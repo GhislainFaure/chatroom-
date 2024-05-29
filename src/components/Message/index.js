ac2e535a7d43754e6aed61eca3a33cc1f5f619a0
GhislainFaure <ghislainfaure06@gmail.com>
2024-05-29 11:36:45 +0200

    mise en place d'un selector pour determiner si l'auteur du message correspond au user connecté

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./style.scss";
 import { useSelector } from "react-redux";
+import { isMessageMine } from "../../selectors";
 
 export default function Message({ author, content }) {
-  const { pseudo } = useSelector((state) => state.messages);
+  // on determine si le message est le notre en comparant l'author avec le user
+  // connecté à l'application (pseudo)
 
   return (
-    <div className={author === pseudo ? "message message--is-mine" : "message"}>
+    <div
+      className={
+        useSelector(isMessageMine(author))
+          ? "message message--is-mine"
+          : "message"
+      }
+    >
       <p className={"message__author"}>{author}</p>
       <p className="message__content">{content}</p>
     </div>
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -7,3 +7,12 @@ export function getHighestId(items) {
   const ids = items.map((item) => item.id);
   return Math.max(...ids);
 }
+
+/**
+ * fonction qui renvoie un selector , qui determine si l'auteur du message correspond à l'utilisateur connecté
+ * @param {string} author auteur du message
+ * @returns selector
+ */
+export function isMessageMine(author) {
+  return (state) => author === state.messages.pseudo;
+}
